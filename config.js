dee0c42a1585f60f5ca3ba989132f96a8574ea8d
Sova Mrowiec <74007010+SovaMr@users.noreply.github.com>
2026-01-22 11:21:29 +0100

    Add personal configuration file config.js

---
+++ config.js
@@ -0,0 +1,18 @@
+/** ───────────────────────────────────────
+        Personal configuration
+        (Safe for personal use)
+    ─────────────────────────────────────── */
+
+const CONFIG = {
+  myEmail: "your@email.com",
+  myExcel: "Your Google Sheet Name",
+
+  myWorks: [
+    { name: "Name1", url: "https://archiveofourown.org/works/xxxxxxx" },
+    { name: "Name2", url: "https://archiveofourown.org/works/xxxxxxx" },
+    { name: "Name3", url: "https://archiveofourown.org/works/xxxxxxx" },
+    { name: "Name4", url: "https://archiveofourown.org/works/xxxxxxx"}
+  ]
+};
+
+
