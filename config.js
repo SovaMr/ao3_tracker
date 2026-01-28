7fc0765c278305b97fd36c0ed05aa8cba554a9b6
Sova Mrowiec <74007010+SovaMr@users.noreply.github.com>
2026-01-28 14:40:37 +0100

    Add fullName array to config.js

--- config.js
+++ config.js
@@ -12,7 +12,14 @@ const CONFIG = {
     { name: "Name2", url: "https://archiveofourown.org/works/xxxxxxx" },
     { name: "Name3", url: "https://archiveofourown.org/works/xxxxxxx" },
     { name: "Name4", url: "https://archiveofourown.org/works/xxxxxxx"}
+  ],
+  fullName: [
+    { name: "fullName1", url: "https://archiveofourown.org/works/xxxxxxx" },
+    { name: "fullName2", url: "https://archiveofourown.org/works/xxxxxxx" },
+    { name: "fullName3", url: "https://archiveofourown.org/works/xxxxxxx" },
+    { name: "fullName4", url: "https://archiveofourown.org/works/xxxxxxx"}
   ]
+   
 };
 
 
